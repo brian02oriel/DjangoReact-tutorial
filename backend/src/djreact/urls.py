9f6663db2aaa25c27a377f92dbc7d42e5e9bbfca
brian02oriel <brian.nieto1@utp.ac.pa>
2019-01-25 14:38:36 -0500

    Vistas genericas usando el api

--- backend/src/djreact/urls.py
+++ backend/src/djreact/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('api/', include('articles.api.urls'))
 ]
